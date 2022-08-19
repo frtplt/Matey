98718c742f414336039ed2a483e00e621d676a31
Fırat Onurcan POLAT <firat_p@hotmail.com>
2022-08-19 08:38:31 +0300

    Core data manager file added
    Core data functions created

--- Matey/Controllers/MainTabBarViewController.swift
+++ Matey/Controllers/MainTabBarViewController.swift
@@ -26,16 +26,16 @@ class MainTabBarViewController: UITabBarController {
     private func uiInit() {
         self.selectedIndex = 1
         
-        self.viewControllers![0].title = "History"
-        self.viewControllers![1].title = "Home"
-        self.viewControllers![2].title = "Profile"
+        self.viewControllers?[0].title = "History"
+        self.viewControllers?[1].title = "Home"
+        self.viewControllers?[2].title = "Profile"
         
-        self.viewControllers![0].tabBarItem.selectedImage = UIImage(systemName: "book")
-        self.viewControllers![0].tabBarItem.image = UIImage(systemName: "book")
-        self.viewControllers![1].tabBarItem.selectedImage = UIImage(systemName: "calendar")
-        self.viewControllers![1].tabBarItem.image = UIImage(systemName: "calendar")
-        self.viewControllers![2].tabBarItem.selectedImage = UIImage(named: "person")
-        self.viewControllers![2].tabBarItem.image = UIImage(named: "person")
+        self.viewControllers?[0].tabBarItem.selectedImage = UIImage(systemName: "book")
+        self.viewControllers?[0].tabBarItem.image = UIImage(systemName: "book")
+        self.viewControllers?[1].tabBarItem.selectedImage = UIImage(systemName: "calendar")
+        self.viewControllers?[1].tabBarItem.image = UIImage(systemName: "calendar")
+        self.viewControllers?[2].tabBarItem.selectedImage = UIImage(named: "person")
+        self.viewControllers?[2].tabBarItem.image = UIImage(named: "person")
     
         self.tabBar.tintColor = UIColor(displayP3Red: 47/255, green: 140/255, blue: 79/255, alpha: 1.0)
         let blurEffect = UIBlurEffect(style: .dark) // here you can change blur style
