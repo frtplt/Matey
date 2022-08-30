2a5e3c1efb8fc6e0158f72991a40b738b4dd5777
Fırat Onurcan POLAT <firat_p@hotmail.com>
2022-08-30 12:31:36 +0300

    minor changes in coredatamanager

--- Matey/CoreDataManager.swift
+++ Matey/CoreDataManager.swift
@@ -12,20 +12,20 @@ import UIKit
 
 protocol CoreDataManagerInterface: AnyObject {
   func saveContext()
-  func insertPerson(name: String, friend: String, lend: String, borrow: String, username: String) -> Person?
-  func update(name: String, friend: String, lend: String, borrow: String, username: String, person: Person)
+    func insertPerson(name: String, friend: String, lend: String, borrow: String, username: String) -> Person?
+      func update(name: String, friend: String, lend: String, borrow: String, username: String, person: Person)
   func delete(person: Person)
 }
 
 class CoreDataManager: CoreDataManagerInterface {
 
    var persistentContainer: NSPersistentContainer = {
-    
+
     let container = NSPersistentContainer(name: "Matey")
-    
-    
+
+
     container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-      
+
       if let error = error as NSError? {
         fatalError("Unresolved error \(error), \(error.userInfo)")
       }
@@ -47,7 +47,7 @@ class CoreDataManager: CoreDataManagerInterface {
     }
   }
 
-  func insertPerson(name: String, friend: String, lend: String, borrow: String, username: String) -> Person? {
+    func insertPerson(name: String, friend: String, lend: String, borrow: String, username: String) -> Person? {
 
     let managedContext = persistentContainer.viewContext
 
@@ -73,11 +73,11 @@ class CoreDataManager: CoreDataManagerInterface {
       return nil
     }
   }
-  
-  func update(name: String, friend: String, lend: String, borrow: String, username: String, person: Person) {
+
+    func update(name: String, friend: String, lend: String, borrow: String, username: String, person: Person) {
 
     let context = persistentContainer.viewContext
-    
+
     do {
       person.setValue(name, forKeyPath: "name")
       person.setValue(friend, forKeyPath: "friend")
@@ -93,19 +93,19 @@ class CoreDataManager: CoreDataManagerInterface {
       } catch let error as NSError  {
         print("Could not save \(error), \(error.userInfo)")
       } catch {
-        
+
       }
-      
+
     } catch {
       print("Error with request: \(error)")
     }
   }
-  
+
   /*delete*/
   func delete(person: Person) {
-    
+
     let managedContext = persistentContainer.viewContext
-    
+
     do {
       managedContext.delete(person)
     } catch {
@@ -118,18 +118,17 @@ class CoreDataManager: CoreDataManagerInterface {
       // Do something in response to error condition
     }
   }
-  
-  func fetchCurrentPerson(username: String) -> [Person]?{
+
+    func fetchCurrentPerson(username: String) -> [Person]?{
     /*Before you can do anything with Core Data, you need a managed object context. */
     let managedContext = persistentContainer.viewContext
-    
+
     /*As the name suggests, NSFetchRequest is the class responsible for fetching from Core Data.
-     
+
      Initializing a fetch request with init(entityName:), fetches all objects of a particular entity. This is what you do here to fetch all Person entities.
      */
     let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
     fetchRequest.predicate = NSPredicate(format: "username == %@" ,username as CVarArg)
-    
     /*You hand the fetch request over to the managed object context to do the heavy lifting. fetch(_:) returns an array of managed objects meeting the criteria specified by the fetch request.*/
     do {
       let people = try managedContext.fetch(fetchRequest)
@@ -139,37 +138,38 @@ class CoreDataManager: CoreDataManagerInterface {
       return nil
     }
   }
-  
+
     func delete(username: String) -> [Person]? {
     /*get reference to appdelegate file*/
-      
+
+
     /*get reference of managed object context*/
     let managedContext = persistentContainer.viewContext
-    
+
     /*init fetch request*/
     let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
-    
+
     /*pass your condition with NSPredicate. We only want to delete those records which match our condition*/
     fetchRequest.predicate = NSPredicate(format: "username == %@" ,username as CVarArg)
     do {
-      
+
       /*managedContext.fetch(fetchRequest) will return array of person objects [personObjects]*/
       let item = try managedContext.fetch(fetchRequest)
       var arrRemovedPeople = [Person]()
       for person in item {
-        
+
         /*call delete method(aManagedObjectInstance)*/
         /*here i is managed object instance*/
         managedContext.delete(person)
-        
+
         /*finally save the contexts*/
         try managedContext.save()
-        
+
         /*update your array also*/
         arrRemovedPeople.append(person as? Person ?? Person())
       }
       return arrRemovedPeople
-      
+
     } catch let error as NSError {
       print("Could not fetch. \(error), \(error.userInfo)")
       return nil
